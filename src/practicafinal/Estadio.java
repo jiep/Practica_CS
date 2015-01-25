74f1656453e7c1e5ce79cb022cd0d60f608ac341
José Ignacio Escribano <jiep12345@hotmail.com>
2015-01-25 10:29:58 +0100

    Corregido error

--- src/practicafinal/Estadio.java
+++ src/practicafinal/Estadio.java
@@ -59,43 +59,6 @@ public class Estadio {
     public int getCapacidad(){
         return this.capacidad;
     }	
-<<<<<<< HEAD
-
-	/**
-	* Amplia la funcionalidad de 
-	* la versi�n anterior con
-	* los estadios.
-	*/
-	
-	private int capacidad;
-	private int estrellas_UEFA;
-	private String nombre;
-	
-	public Estadio(){
-		this.capacidad = 0;
-		this.estrellas_UEFA = 0;
-		this.nombre = "";
-	}
-	
-	public Estadio(int capacidad, int estrellas_UEFA, String nombre){
-		this.capacidad = capacidad;
-		this.estrellas_UEFA = estrellas_UEFA;
-		this.nombre = nombre;
-	}
-	
-	public int getEstrellas_UEFA(){
-		return this.estrellas_UEFA;
-	}
-	
-	public String getNombre(){
-		return this.nombre;
-	}
-	
-	public int getCapacidad(){
-		return this.capacidad;
-	}	
-
-=======
     
     public void addPersonal (Directivo directivo){
         this.personal.add(directivo);
@@ -111,5 +74,4 @@ public class Estadio {
         }
         return serie;
     }
->>>>>>> origin/PersonalEstadio
 }
